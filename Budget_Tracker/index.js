69dc56f27036efeb235dc77a0dc54a0e91f72299
Josh Burton <jburton175@gmail.com>
2024-11-13 10:43:06 -0700

    Your commit message

---
+++ Budget_Tracker/index.js
@@ -0,0 +1,41 @@
+let express = require("express");
+
+let app = express();
+
+let path = require("path");
+
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(__dirname + '/views'));
+
+app.use(express.urlencoded({extended: true}));
+
+// connect to pg 
+const knex = require("knex") ({
+    client : "pg",
+    connection : {
+        host : "localhost",
+        user : "postgres",
+        password : "admin",
+        database : "budget_tracker",
+        port : 5432
+    }
+});
+
+
+
+app.get("/", (req, res) =>
+    {
+        knex.select().from('transactions').orderBy('transaction_id').then( transactions => {
+            res.render("index", { transaction: transactions });
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json({err});
+        });
+    });
+
+    
+app.listen(port, () => console.log('listening'));
