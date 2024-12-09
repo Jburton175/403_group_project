10f069bf5dc28c8b99d85d933d77fcc5b79d7c68
JBurton175 <jburton175@gmail.com>
2024-12-09 09:44:17 -0700

    add login - needs updates still

--- Budget_Tracker/index.js
+++ Budget_Tracker/index.js
@@ -7,6 +7,8 @@ let app = express();
 
 let path = require("path");
 
+let security = false;
+
 const port = 3000;
 
 app.set("view engine", "ejs");
@@ -28,29 +30,54 @@ const knex = require("knex") ({
     }
 });
 
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    try {
+        // Query the user table to find the record
+        const user = knex('user')
+            .select('*')
+            .where({ username, password }) // Replace with hashed password comparison in production
+            .first(); // Returns the first matching record
+        if (user) {
+            security = true;
+        } else {
+            security = false;
+        }
+    } catch (error) {
+        res.status(500).send('Database query failed: ' + error.message);
+    }
+    res.redirect("/")
+});
+
+
 app.get("/", (req, res) =>
     {
-        res.render("index");
-    }
-);
+        res.render('index', { security });
+    });
+    // .catch(error => {
+    //   console.error('Error querying database:', error);
+    //   res.status(500).send('Internal Server Error');
+    // });
+    
 
 
 app.get("/home", (req, res) =>
     {
-        res.render("home");
+        res.render("home", { security });
         
     });
 
 app.get("/budgets", (req, res) =>
     {
-        res.render("budget");
+        res.render("budget", { security });
         
     });
 
 app.get("/transactions", (req, res) =>
     {
         knex.select().from('transactions').orderBy('transaction_id').then( transactions => {
-            res.render("transactions", { transaction: transactions });
+            res.render("transactions", { transaction: transactions, security });
         }).catch(err => {
             console.log(err);
             res.status(500).json({err});
